9cbcd9c37e9b076885f5d802f1d74cc92ba88ed3
Harry Torry <Harrytorry2@gmail.com>
2012-12-30 03:31:33 +0000

    Changed ResourceNode render to resource.png

--- src/mapgen/ResourceNode.java
+++ src/mapgen/ResourceNode.java
@@ -22,6 +22,7 @@ package mapgen;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -30,6 +31,8 @@ public class ResourceNode {
     int x, y;
     int income_per_tick;
     boolean rich = false;
+    Image _image;
+    float scale;
 
     ResourceNode(int x, int y, boolean rich) {
 
@@ -38,10 +41,13 @@ public class ResourceNode {
 
         if (rich) {
             income_per_tick = 5;
+            scale = 2f;
         } else {
             income_per_tick = 10;
+            scale = 1f;
         }
 
+        _image = game.ResourceManager.getInstance().getImage("ResourceAnimation");
 
     }
     
@@ -54,14 +60,8 @@ public class ResourceNode {
 
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         g.setColor(Color.orange);
-        if (rich) {
-            g.fillRect(x, y, 50, 50);
-            g.setColor(Color.white);
-            g.fillRect(x + 12, y + 12, 25, 25);
-        } else {
-            g.fillRect(x, y, 25, 25);
-            g.setColor(Color.white);
-            g.fillRect(x + 7, y + 7, 12, 12);
-        }
+        
+        
+        _image.draw(x, y, scale);
     }
 }

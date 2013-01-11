143efc2f217033d4a5de057a82fa77f5cfa18b20
Harry Torry <Harrytorry2@gmail.com>
2013-01-11 21:50:44 +0000

    Massive overhaul
    Everything is new, basically.

--- src/lighting/LightManager.java
+++ src/lighting/LightManager.java
@@ -19,27 +19,33 @@
  */
 package lighting;
 
-import game.ResourceManager;
+import java.awt.Point;
+
 import java.util.ArrayList;
-import java.util.Random;
-import mapgen.MapTile;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Ellipse;
+import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.geom.Rectangle;
 
 public class LightManager {
 
     ArrayList<Light> lights = new ArrayList<>();
     float[][][] default_light_value;
     float[][][] calculated_light_value;
+    Shape circle = new Ellipse(0f, 0f, 40f, 40f, 6);
+    Shape[][] test; // The hitboxes for each square
+    mapgen.MapTile[][] tiles;
 
     // Maps is 53x50
     public LightManager(mapgen.MapTile[][] tile) {
 
         default_light_value = new float[53][50][];
+        tiles = tile;
 
         for (int y = 0; y < 50; y++) {
             for (int x = 0; x < 53; x++) {
@@ -47,7 +53,14 @@ public class LightManager {
             }
         }
 
+        int tile_size = 16;
+        test = new Rectangle[53][50];
+        for (int y = 0; y < 50; y++) {
+            for (int x = 0; x < 53; x++) {
+                test[x][y] = new Rectangle((x * 16) + 8, (y * 16) + 8, 2, 2);
+            }
 
+        }
     }
 
     public void UpdateDefaultTile(mapgen.MapTile tile) {
@@ -56,44 +69,58 @@ public class LightManager {
 
     public void update(GameContainer container, StateBasedGame game, int delta, ArrayList<Light> lights) throws SlickException {
 
+        for (int y = 0; y < 50; y++) {
+            for (int x = 0; x < 53; x++) {
+                default_light_value[x][y][0] = 0;
+            }
+        }
+
         calculated_light_value = default_light_value;
         this.lights = lights;
 
-
-
-
-    }
-
-    public void render(GameContainer container, Graphics g) throws SlickException {
-
-        // g.setDrawMode(Graphics.MODE_ALPHA_BLEND);
+        Input input = container.getInput();
+        circle.setLocation(input.getMouseX() - 10f, input.getMouseY() - 10f);
 
 
 
         for (int y = 0; y < 50; y++) {
             for (int x = 0; x < 53; x++) {
+                if (circle.contains(test[x][y])) {
+                    calculated_light_value[x][y][0] = 2f;
+                }
+            }
+        }
+    }
 
+    public void render(GameContainer container, Graphics g) throws SlickException {
+
+        //g.setDrawMode(Graphics.MODE_ALPHA_BLEND);
+
+
+        g.fill(circle);
+
+        for (int y = 0; y < 50; y++) {
+            for (int x = 0; x < 53; x++) {
                 g.setColor(Color.black);
                 float value = default_light_value[x][y][0];
                 String str = "a";
                 if (value == 0.5f) {
                     str = "0";
-                } else {
+                }
+                if (value == 1.0f) {
                     str = "1";
                 }
-
+                if (value == 2.0f) {
+                    str = "2";
+                }
                 g.drawString(str, (x * 16) + 3, (y * 16) + 0);
-
             }
         }
 
-        System.out.println(lights.size());
-
-        for (int i = 0; i<lights.size(); i++) {
+        for (int i = 0; i < lights.size(); i++) {
             Light light = lights.get(i);
             g.drawOval(light.x, light.y, light.size, light.size);
         }
-   
 
         g.setDrawMode(Graphics.MODE_NORMAL);
     }

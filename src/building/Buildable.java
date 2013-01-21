8018a417cd4a354f309f368d8b71e3a026e22c04
Harry Torry <Harrytorry@gmail.com>
2013-01-21 06:54:29 +0000

    Added base image and update/render

--- src/building/Buildable.java
+++ src/building/Buildable.java
@@ -19,31 +19,42 @@
  */
 package building;
 
+import game.ResourceManager;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Buildable {
 
     float x, y;
     int w, h, cost;
     Image baseImage;
-
     public static final int BUILDING_BARRACKS = 1;
-    
-    
+
     Buildable(int type, int x, int y) {
         this.x = x;
         this.y = y;
-        
-        switch(type) {
-            case BUILDING_BARRACKS: 
+
+        switch (type) {
+            case BUILDING_BARRACKS:
+                baseImage = ResourceManager.getInstance().getImage("ingame_building_barrack");
                 break;
-            
+
         }
-        
-        
-    
+
+
+
     }
-    
+
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        // Create new unit
+    }
+
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        baseImage.draw(x, y);
+    }
 
     public int getH() {
         return h;
